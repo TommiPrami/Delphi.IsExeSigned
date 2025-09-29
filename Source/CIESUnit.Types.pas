149a7d35a0a6610c7c0440907d418657119b2dcb
TommiPrami <tommi.prami@gmail.com>
2025-09-29 10:28:10 +0300

    Initial commit of code

---
+++ Source/CIESUnit.Types.pas
@@ -0,0 +1,105 @@
+﻿unit CIESUnit.Types;
+
+interface
+
+uses
+  Winapi.Windows;
+
+{
+  Types, Conmsts and WinApi declarations.
+}
+
+const
+  CERT_QUERY_OBJECT_FILE = $00000001;
+  CERT_QUERY_CONTENT_FLAG_PKCS7_SIGNED_EMBED = $00000400;
+  CERT_QUERY_FORMAT_FLAG_BINARY = $00000002;
+  CMSG_SIGNER_INFO_PARAM = 6;
+  CERT_NAME_SIMPLE_DISPLAY_TYPE = 4;
+  CERT_FIND_SUBJECT_CERT = 655360;
+  X509_ASN_ENCODING = $00000001;
+  PKCS_7_ASN_ENCODING = $00010000;
+
+type
+  HCERTSTORE = Pointer;
+  HCRYPTMSG = Pointer;
+  PCCERT_CONTEXT = Pointer;
+
+  CRYPT_DATA_BLOB = record
+    cbData: DWORD;
+    pbData: PByte;
+  end;
+  CRYPT_INTEGER_BLOB = CRYPT_DATA_BLOB;
+  CERT_NAME_BLOB = CRYPT_DATA_BLOB;
+
+  PCMSG_SIGNER_INFO = ^CMSG_SIGNER_INFO;
+  CMSG_SIGNER_INFO = record
+    dwVersion: DWORD;
+    Issuer: CERT_NAME_BLOB;
+    SerialNumber: CRYPT_INTEGER_BLOB;
+    HashAlgorithm: record
+      pszObjId: PAnsiChar;
+      Parameters: CRYPT_DATA_BLOB;
+    end;
+    HashEncryptionAlgorithm: record
+      pszObjId: PAnsiChar;
+      Parameters: CRYPT_DATA_BLOB;
+    end;
+    EncryptedHash: CRYPT_DATA_BLOB;
+    AuthAttrs: record
+      cAttr: DWORD;
+      rgAttr: Pointer;
+    end;
+    UnauthAttrs: record
+      cAttr: DWORD;
+      rgAttr: Pointer;
+    end;
+  end;
+
+  CERT_INFO = record
+    dwVersion: DWORD;
+    SerialNumber: CRYPT_INTEGER_BLOB;
+    SignatureAlgorithm: record
+      pszObjId: PAnsiChar;
+      Parameters: CRYPT_DATA_BLOB;
+    end;
+    Issuer: CERT_NAME_BLOB;
+    NotBefore: TFileTime;
+    NotAfter: TFileTime;
+    Subject: CERT_NAME_BLOB;
+    SubjectPublicKeyInfo: record
+      Algorithm: record
+        pszObjId: PAnsiChar;
+        Parameters: CRYPT_DATA_BLOB;
+      end;
+      PublicKey: CRYPT_DATA_BLOB;
+    end;
+    IssuerUniqueId: CRYPT_DATA_BLOB;
+    SubjectUniqueId: CRYPT_DATA_BLOB;
+    cExtension: DWORD;
+    rgExtension: Pointer;
+  end;
+  PCERT_INFO = ^CERT_INFO;
+
+function CryptQueryObject(dwObjectType: DWORD; pvObject: Pointer; dwExpectedContentTypeFlags: DWORD; dwExpectedFormatTypeFlags: DWORD;
+  dwFlags: DWORD; pdwMsgAndCertEncodingType: PDWORD; pdwContentType: PDWORD; pdwFormatType: PDWORD;
+  phCertStore: Pointer; phMsg: Pointer; ppvContext: Pointer): BOOL; stdcall; external 'Crypt32.dll';
+
+function CertCloseStore(hCertStore: HCERTSTORE; dwFlags: DWORD): BOOL; stdcall; external 'Crypt32.dll';
+
+function CryptMsgClose(hCryptMsg: HCRYPTMSG): BOOL; stdcall; external 'Crypt32.dll';
+
+function CryptMsgGetParam(hCryptMsg: HCRYPTMSG; dwParamType: DWORD; dwIndex: DWORD; pvData: Pointer; pcbData: PDWORD): BOOL; stdcall; external 'Crypt32.dll';
+
+function CertFindCertificateInStore(hCertStore: HCERTSTORE; dwCertEncodingType: DWORD; dwFindFlags: DWORD; dwFindType: DWORD;
+  pvFindPara: Pointer; pPrevCertContext: PCCERT_CONTEXT): PCCERT_CONTEXT; stdcall; external 'Crypt32.dll';
+
+function CertFreeCertificateContext(pCertContext: PCCERT_CONTEXT): BOOL; stdcall; external 'Crypt32.dll';
+
+function CertGetNameString(pCertContext: PCCERT_CONTEXT; dwType: DWORD; dwFlags: DWORD; pvTypePara: Pointer; pszNameString: PChar;
+  cchNameString: DWORD): DWORD; stdcall; external 'Crypt32.dll' name 'CertGetNameStringW';
+
+
+implementation
+
+
+end.
---
+++ Source/CIESUnit.Utils.pas
@@ -0,0 +1,116 @@
+﻿unit CIESUnit.Utils;
+
+interface
+
+  function IsExeSigned(const AExeFileName: string): Boolean;
+  function GetSignerName(const AExeFileName: string): string;
+
+implementation
+
+uses
+  Winapi.Windows, System.SysUtils, CIESUnit.Types;
+
+function IsExeSigned(const AExeFileName: string): Boolean;
+var
+  dwEncoding, dwContentType, dwFormatType: DWORD;
+begin
+  Result := False;
+
+  if not FileExists(AExeFileName) then
+    Exit;
+
+  var hFile := CreateFile(PChar(AExeFileName), GENERIC_READ, FILE_SHARE_READ, nil, OPEN_EXISTING, 0, 0);
+
+  if hFile = INVALID_HANDLE_VALUE then
+    Exit;
+
+  var hStore: HCERTSTORE := nil;
+  var hMsg: HCRYPTMSG := nil;
+  try
+    // Check if the file has an embedded signature
+    Result := CryptQueryObject(CERT_QUERY_OBJECT_FILE, PChar(AExeFileName), CERT_QUERY_CONTENT_FLAG_PKCS7_SIGNED_EMBED,
+      CERT_QUERY_FORMAT_FLAG_BINARY, 0, @dwEncoding, @dwContentType, @dwFormatType, @hStore, @hMsg, nil);
+  finally
+    if Result then
+    begin
+      if Assigned(hStore) then
+        CertCloseStore(hStore, 0);
+
+      if Assigned(hMsg) then
+        CryptMsgClose(hMsg);
+    end;
+
+    CloseHandle(hFile);
+  end;
+end;
+
+function GetSignerName(const AExeFileName: string): string;
+var
+  dwEncoding, dwContentType, dwFormatType, cbSignerInfo: DWORD;
+  hStore: HCERTSTORE;
+begin
+  Result := '';
+
+  if not FileExists(AExeFileName) then
+    Exit;
+
+  var LhMsg: HCRYPTMSG;
+
+  if not CryptQueryObject(CERT_QUERY_OBJECT_FILE, PChar(AExeFileName), CERT_QUERY_CONTENT_FLAG_PKCS7_SIGNED_EMBED,
+    CERT_QUERY_FORMAT_FLAG_BINARY, 0, @dwEncoding, @dwContentType, @dwFormatType, @hStore, @LhMsg, nil) then
+    Exit;
+
+  try
+    // Get signer information size
+    cbSignerInfo := 0;
+    CryptMsgGetParam(LhMsg, CMSG_SIGNER_INFO_PARAM, 0, nil, @cbSignerInfo);
+
+    if cbSignerInfo = 0 then
+      Exit;
+
+    var pSignerInfo: PCMSG_SIGNER_INFO;
+    GetMem(pSignerInfo, cbSignerInfo);
+    try
+      // Get signer information
+      if not CryptMsgGetParam(LhMsg, CMSG_SIGNER_INFO_PARAM, 0, pSignerInfo, @cbSignerInfo) then
+        Exit;
+
+      // Prepare CERT_INFO structure to search for the certificate
+      var LCertInfo: CERT_INFO;
+      FillChar(LCertInfo, SizeOf(LCertInfo), 0);
+      LCertInfo.Issuer := pSignerInfo.Issuer;
+      LCertInfo.SerialNumber := pSignerInfo.SerialNumber;
+
+      // Search for the signer certificate in the store
+      var pCertContext: PCCERT_CONTEXT;
+      pCertContext := CertFindCertificateInStore(hStore, X509_ASN_ENCODING or PKCS_7_ASN_ENCODING, 0, CERT_FIND_SUBJECT_CERT,
+        @LCertInfo, nil);
+
+      if pCertContext <> nil then
+      begin
+        try
+          var LdwData: DWORD;
+          var LszName: array[0..255] of Char;
+
+          // Get the subject name
+          LdwData := CertGetNameString(pCertContext, CERT_NAME_SIMPLE_DISPLAY_TYPE, 0, nil, @LszName[0], Length(LszName));
+
+          if LdwData > 1 then
+            Result := LszName;
+        finally
+          CertFreeCertificateContext(pCertContext);
+        end;
+      end;
+    finally
+      FreeMem(pSignerInfo);
+    end;
+  finally
+    if hStore <> nil then
+      CertCloseStore(hStore, 0);
+    if LhMsg <> nil then
+      CryptMsgClose(LhMsg);
+  end;
+end;
+
+
+end.
