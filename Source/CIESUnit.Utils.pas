0493e195e194fefb00f9adeb15eded2ebd140595
TommiPrami <tommi.prami@gmail.com>
2026-06-30 11:21:17 +0300

    Fixed naming and name lenght fix

--- Source/CIESUnit.Utils.pas
+++ Source/CIESUnit.Utils.pas
@@ -12,103 +12,98 @@ uses
 
 function IsExeSigned(const AExeFileName: string): Boolean;
 var
-  dwEncoding, dwContentType, dwFormatType: DWORD;
+  LEncoding: DWORD;
+  LContentType: DWORD;
+  LFormatType: DWORD;
 begin
   Result := False;
 
   if not FileExists(AExeFileName) then
     Exit;
 
-  var hFile := CreateFile(PChar(AExeFileName), GENERIC_READ, FILE_SHARE_READ, nil, OPEN_EXISTING, 0, 0);
-
-  if hFile = INVALID_HANDLE_VALUE then
-    Exit;
-
-  var hStore: HCERTSTORE := nil;
-  var hMsg: HCRYPTMSG := nil;
+  var LStore: HCERTSTORE := nil;
+  var LMsg: HCRYPTMSG := nil;
   try
     // Check if the file has an embedded signature
     Result := CryptQueryObject(CERT_QUERY_OBJECT_FILE, PChar(AExeFileName), CERT_QUERY_CONTENT_FLAG_PKCS7_SIGNED_EMBED,
-      CERT_QUERY_FORMAT_FLAG_BINARY, 0, @dwEncoding, @dwContentType, @dwFormatType, @hStore, @hMsg, nil);
+      CERT_QUERY_FORMAT_FLAG_BINARY, 0, @LEncoding, @LContentType, @LFormatType, @LStore, @LMsg, nil);
   finally
-    if Result then
-    begin
-      if Assigned(hStore) then
-        CertCloseStore(hStore, 0);
+    if Assigned(LStore) then
+      CertCloseStore(LStore, 0);
 
-      if Assigned(hMsg) then
-        CryptMsgClose(hMsg);
-    end;
-
-    CloseHandle(hFile);
+    if Assigned(LMsg) then
+      CryptMsgClose(LMsg);
   end;
 end;
 
 function GetSignerName(const AExeFileName: string): string;
-var
-  dwEncoding, dwContentType, dwFormatType, cbSignerInfo: DWORD;
-  hStore: HCERTSTORE;
 begin
   Result := '';
 
   if not FileExists(AExeFileName) then
     Exit;
 
-  var LhMsg: HCRYPTMSG;
+  var LStore: HCERTSTORE := nil;
+  var LMsg: HCRYPTMSG := nil;
+  var LEncoding: DWORD;
+  var LContentType: DWORD;
+  var LFormatType: DWORD;
 
   if not CryptQueryObject(CERT_QUERY_OBJECT_FILE, PChar(AExeFileName), CERT_QUERY_CONTENT_FLAG_PKCS7_SIGNED_EMBED,
-    CERT_QUERY_FORMAT_FLAG_BINARY, 0, @dwEncoding, @dwContentType, @dwFormatType, @hStore, @LhMsg, nil) then
+    CERT_QUERY_FORMAT_FLAG_BINARY, 0, @LEncoding, @LContentType, @LFormatType, @LStore, @LMsg, nil) then
     Exit;
 
   try
     // Get signer information size
-    cbSignerInfo := 0;
-    CryptMsgGetParam(LhMsg, CMSG_SIGNER_INFO_PARAM, 0, nil, @cbSignerInfo);
+    var LSignerInfoSize: DWORD := 0;
+    CryptMsgGetParam(LMsg, CMSG_SIGNER_INFO_PARAM, 0, nil, @LSignerInfoSize);
 
-    if cbSignerInfo = 0 then
+    if LSignerInfoSize = 0 then
       Exit;
 
-    var pSignerInfo: PCMSG_SIGNER_INFO;
-    GetMem(pSignerInfo, cbSignerInfo);
+    var LSignerInfo: PCMSG_SIGNER_INFO;
+    GetMem(LSignerInfo, LSignerInfoSize);
     try
       // Get signer information
-      if not CryptMsgGetParam(LhMsg, CMSG_SIGNER_INFO_PARAM, 0, pSignerInfo, @cbSignerInfo) then
+      if not CryptMsgGetParam(LMsg, CMSG_SIGNER_INFO_PARAM, 0, LSignerInfo, @LSignerInfoSize) then
         Exit;
 
       // Prepare CERT_INFO structure to search for the certificate
       var LCertInfo: CERT_INFO;
       FillChar(LCertInfo, SizeOf(LCertInfo), 0);
-      LCertInfo.Issuer := pSignerInfo.Issuer;
-      LCertInfo.SerialNumber := pSignerInfo.SerialNumber;
+      LCertInfo.Issuer := LSignerInfo.Issuer;
+      LCertInfo.SerialNumber := LSignerInfo.SerialNumber;
 
       // Search for the signer certificate in the store
-      var pCertContext: PCCERT_CONTEXT;
-      pCertContext := CertFindCertificateInStore(hStore, X509_ASN_ENCODING or PKCS_7_ASN_ENCODING, 0, CERT_FIND_SUBJECT_CERT,
+      var LCertContext: PCCERT_CONTEXT;
+      LCertContext := CertFindCertificateInStore(LStore, X509_ASN_ENCODING or PKCS_7_ASN_ENCODING, 0, CERT_FIND_SUBJECT_CERT,
         @LCertInfo, nil);
 
-      if pCertContext <> nil then
+      if LCertContext <> nil then
       begin
         try
-          var LdwData: DWORD;
-          var LszName: array[0..255] of Char;
+          // Get the subject name length (in characters, including the terminating null)
+          var LNameLen := CertGetNameString(LCertContext, CERT_NAME_SIMPLE_DISPLAY_TYPE, 0, nil, nil, 0);
 
-          // Get the subject name
-          LdwData := CertGetNameString(pCertContext, CERT_NAME_SIMPLE_DISPLAY_TYPE, 0, nil, @LszName[0], Length(LszName));
-
-          if LdwData > 1 then
-            Result := LszName;
+          if LNameLen > 1 then
+          begin
+            SetLength(Result, LNameLen);
+            LNameLen := CertGetNameString(LCertContext, CERT_NAME_SIMPLE_DISPLAY_TYPE, 0, nil, PChar(Result), LNameLen);
+            // Drop the terminating null that CertGetNameString counts in the length
+            SetLength(Result, LNameLen - 1);
+          end;
         finally
-          CertFreeCertificateContext(pCertContext);
+          CertFreeCertificateContext(LCertContext);
         end;
       end;
     finally
-      FreeMem(pSignerInfo);
+      FreeMem(LSignerInfo);
     end;
   finally
-    if hStore <> nil then
-      CertCloseStore(hStore, 0);
-    if LhMsg <> nil then
-      CryptMsgClose(LhMsg);
+    if Assigned(LStore) then
+      CertCloseStore(LStore, 0);
+    if Assigned(LMsg) then
+      CryptMsgClose(LMsg);
   end;
 end;
 
